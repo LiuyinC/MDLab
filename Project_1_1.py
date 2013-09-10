5ca8380d41c1c83863241d634b91bbc8361b03c1
titan <yuxiangkou@dhcp-140-254-181-112.osuwireless.ohio-state.edu>
2013-09-10 12:02:28 -0400

    create a function to get frequency distribution of given string
    def countWords(data):
        # tokens = nltk.tokenize.word_tokenize(data)
        tokens = preprocess(data)
        fdist = FreqDist(tokens) #input is a list of string
        return fdist

--- Project_1_1.py
+++ Project_1_1.py
@@ -5,6 +5,7 @@ from lxml import html
 from lxml import etree
 from nltk.corpus import stopwords
 from lab1_sgm import *
+from nltk import FreqDist
 
 PUNCTUATION =[';', ':', ',', '.', '!', '?', '>', '<']
 GENERAL_STOPWORDS_LIST = stopwords.words("english") + PUNCTUATION
@@ -16,25 +17,24 @@ def preprocess(paragraph): # Tokenize, stem, remover general stop words
     stemmer = stem.snowball.EnglishStemmer()
     for word in tokenized_paragraph:
         stemmed_paragraph.append(stemmer.stem(word))
-    preprocessed_paragragh = list(set(stemmed_paragraph) - set(GENERAL_STOPWORDS_LIST))
+    # we don't want to merge repeated words, so that we could not use set
+    # preprocessed_paragragh = list(set(stemmed_paragraph) - set(GENERAL_STOPWORDS_LIST))
+    preprocessed_paragragh = [x for x in stemmed_paragraph if x not in GENERAL_STOPWORDS_LIST]
     return preprocessed_paragragh
 
 class Article:
-    def __init__(self,text_id,title,body):
+    def __init__(self,text_id,title,content):
         self.text_id = text_id
         self.title = title
-        self.body = body
+        self.content = content
     def title_keywords(self):
-        return preprocess(self.title)
+        return set(preprocess(self.title))
 
-def article_reader():
+def article_reader(filename):
     articles_list = []
-    sgm_extractor = SgmFile("./rawdata/reut2-000.sgm")
-    # print len(sgm_extractor.articles)
+    sgm_extractor = SgmFile(filename)
     articles_amount = xrange(len(sgm_extractor.articles))
     articles_list=[Article(tid, sgm_extractor.articles[tid].title, sgm_extractor.articles[tid].content) for tid in articles_amount]
-    # for i in xrange(len(sgm_extractor.articles)):
-    #     print simplelist[i].title
     return articles_list
 
 def title_keyword_vector_generator(articles_list):
@@ -44,9 +44,30 @@ def title_keyword_vector_generator(articles_list):
         title_keywords_vector.update({articles_list[aid].text_id : articles_list[aid].title_keywords()})
     return title_keywords_vector
 
-articles_list = article_reader()
+# get the frequency distribution of given string
+def countWords(data):
+    # tokens = nltk.tokenize.word_tokenize(data)
+    tokens = preprocess(data)
+    fdist = FreqDist(tokens) #input is a list of string
+    # print "type fdist:", fdist
+    # vocabulary = fdist.keys() 
+    # print vocabulary[:50]
+    # print "# York: ", fdist['york']
+    # fdist.plot(100, title="hello", cumulative=False)
+    # print fdist.tabulate()
+    # FreqDist: http://nltk.googlecode.com/svn/trunk/doc/api/nltk.probability.FreqDist-class.html
+    return fdist
+
+# NOTE: we need to replace <body> and </body> tags in all *.sgm files
+articles_list = article_reader("./rawdata/reut2-000.sgm")
 title_keywords_vector = title_keyword_vector_generator(articles_list)
+fdist_content = countWords(articles_list[0].content)
 
 # print len(articles_list)
-print articles_list[1].title
-print title_keywords_vector
+print articles_list[0].title
+# print articles_list[0].content
+# print title_keywords_vector
+print "# York: ", fdist_content['york']
+print "top 10 Frequency:\n", fdist_content.tabulate(10)
+
+
