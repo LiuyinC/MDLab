296c554c22070bdab67c170ab55dc8b918d318aa
titan <yuxiangkou@Yuxiang-KoumatoMacBook-Pro.local>
2013-09-13 23:12:45 -0400

    create content_keywords_generator(), rename old content_keywords_generator() to content_FreqDist_generator()
    the idea is:
    first generate sample keywords based on the first 1000 articles, then
    use these sample to generate keywords of all articles.

--- Project_1_1.py
+++ Project_1_1.py
@@ -7,7 +7,7 @@ from nltk.corpus import stopwords
 from lab1_sgm import *
 from nltk import FreqDist
 
-PUNCTUATION =[';', ':', ',', '.', '!', '?', '>', '<', '"', ']', '[']
+PUNCTUATION =[';', ':', ',', '.', '!', '?', '>', '<', '"', ']', '[', '-']
 GENERAL_STOPWORDS_LIST = stopwords.words("english") + PUNCTUATION
 
 def preprocess(paragraph): # Tokenize, stem, remover general stop words
@@ -45,11 +45,12 @@ class Article:
     def content_freqDist(self):
         return count_words(self.content)
 
-def article_reader(filename):
+def article_reader(filename, file_id):
     articles_list = []
     sgm_extractor = SgmFile(filename)
+    prev_article_count = file_id * 1000
     articles_amount = xrange(len(sgm_extractor.articles))
-    articles_list=[Article(tid, sgm_extractor.articles[tid].title, sgm_extractor.articles[tid].content) for tid in articles_amount]
+    articles_list=[Article(tid + prev_article_count, sgm_extractor.articles[tid].title, sgm_extractor.articles[tid].content) for tid in articles_amount]
     return articles_list
 
 def title_keyword_vector_generator(articles_list):
@@ -59,7 +60,8 @@ def title_keyword_vector_generator(articles_list):
         title_keywords_vector.update({articles_list[aid].text_id : articles_list[aid].title_keywords()})
     return title_keywords_vector
 
-def content_keyword_vector_generator(articles_list):
+def content_FreqDist_generator(articles_list):
+    # get the FreqDist of all articles
     all_fdist = FreqDist()
     for article in articles_list:
         for item in article.content_freqDist().iteritems():
@@ -85,19 +87,45 @@ def count_words(data):
 
 def read_all_files():
     articles_list = []
-    for i in xrange(1):
+    for i in xrange(22):
         filename = "./rawdata/reut2-" + str(i).zfill(3) + ".sgm"
-        articles_list += article_reader(filename)
+        articles_list += article_reader(filename, i)
     return articles_list
+
+def sample_content_keywords_generator():
+    # generate sample content keywords by the first .sgm file (1000 articles)
+    sample_articles = article_reader("./rawdata/reut2-000.sgm", 0)
+    sample_fdist = content_FreqDist_generator(sample_articles)
+    relative_word_list = []
+    for word, word_fdist in sample_fdist.items():
+        if word_fdist <=600 and word_fdist >= 100:
+            relative_word_list.append(word)
+    return relative_word_list
+
+def content_keywords_generator(relative_word_list, articles_list):
+    # generate all content keywords based on sample content keywords
+    content_relative_word_vector = {}
+    for article in articles_list:
+        article_id = article.text_id
+        article_relative_word_counter = []
+        article_content = preprocess(article.content)
+        for word in relative_word_list:
+            word_counter = article_content.count(word)
+            article_relative_word_counter.append(word_counter)
+        content_relative_word_vector.update({article_id : article_relative_word_counter})
+    return content_relative_word_vector
+
+
 
 # NOTE: we need to replace <body> and </body> tags in all *.sgm files
 articles_list = read_all_files()
 print "len articles_list:", len(articles_list)
+content_keywords_vector = content_keywords_generator(sample_content_keywords_generator(), articles_list)
 title_keywords_vector = title_keyword_vector_generator(articles_list)
 # print "len articles_list[0].freqDist: ", len(fdist_content.items())
-all_fdist = content_keyword_vector_generator(articles_list)
+# all_fdist = content_FreqDist_generator(articles_list)
 # all_fdist.plot(100, title="hello", cumulative=False)
-print "top 20 Frequency:\n", all_fdist.tabulate(20)
+# print "top 20 Frequency:\n", all_fdist.tabulate(20)
 
 # print "# York: ", fdist_content['york']
 # print "fdist item: ", fdist_content.items()[0]
