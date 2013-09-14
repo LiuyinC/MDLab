7c806aa41d2d21295de24564d9bb6879cc00ec27
LiuyinC <cliuyin@gmail.com>
2013-09-13 20:34:38 -0400

    Merge branch 'master' of https://github.com/LiuyinC/MDLab
    Conflicts:
    	Project_1_1.py

--- Project_1_1.py
+++ Project_1_1.py
@@ -12,12 +12,7 @@ GENERAL_STOPWORDS_LIST = stopwords.words("english") + PUNCTUATION
 
 def preprocess(paragraph): # Tokenize, stem, remover general stop words
     from nltk import stem
-    stop_symbols = "!@#$%^&*()_+=-?></.,';\":][}{\r\n\t~`"
-    tokenized_paragraph = paragraph
-    for s in stop_symbols:
-        tokenized_paragraph = tokenized_paragraph.replace(s, '')
-    tokenized_paragraph = tokenized_paragraph.lower()
-    tokenized_paragraph = tokenized_paragraph.split()
+    tokenized_paragraph = nltk.word_tokenize(paragraph)
     stemmed_paragraph =[]
     stemmer = stem.snowball.EnglishStemmer()
     for word in tokenized_paragraph:
@@ -43,13 +38,11 @@ def article_reader(filename):
     sgm_extractor = SgmFile(filename)
     articles_amount = xrange(len(sgm_extractor.articles))
     articles_list=[Article(tid, sgm_extractor.articles[tid].title, sgm_extractor.articles[tid].content) for tid in articles_amount]
-    print "in reader", len(articles_list)
     return articles_list
 
 def title_keyword_vector_generator(articles_list):
     title_keywords_vector={}
     articles_amount = xrange(len(articles_list))
-    print articles_amount
     for aid in articles_amount:
         title_keywords_vector.update({articles_list[aid].text_id : articles_list[aid].title_keywords()})
     return title_keywords_vector
@@ -58,18 +51,21 @@ def content_keyword_vector_generator(articles_list):
     all_fdist = FreqDist()
     for article in articles_list:
         for item in article.content_freqDist().iteritems():
-            all_fdist.__setitem__(item[0], item[1])
+            all_fdist.inc(item[0], item[1])
     return all_fdist
 
 
 # get the frequency distribution of given string
 def count_words(data):
-    # tokens = nltk.tokenize.word_tokenize(data)
-    
     tokens = preprocess(data)
+    # word_list = {}
+    # for item in set(tokens):
+    #     # word_count = tokens.count(item)
+    #     # word_list = jdif
+    #     pass
+
     # here we want to try to set the tokens
     # tokens = list(set(preprocess(data)))
-
     fdist = FreqDist(tokens) #input is a list of string
     # print "type fdist:", fdist
     # vocabulary = fdist.keys() 
@@ -82,28 +78,27 @@ def count_words(data):
 
 def read_all_files():
     articles_list = []
-    for i in xrange(2):
+    for i in xrange(1):
         filename = "./rawdata/reut2-" + str(i).zfill(3) + ".sgm"
-        print len(articles_list)
         articles_list += article_reader(filename)
-        print len(articles_list)
     return articles_list
+
 
 # NOTE: we need to replace <body> and </body> tags in all *.sgm files
 # articles_list = article_reader("./rawdata/reut2-000.sgm")
 articles_list = read_all_files()
 print "len articles_list:", len(articles_list)
-title_keywords_vector = title_keyword_vector_generator(articles_list)
-print title_keywords_vector
-# fdist_content = articles_list[0].content_freqDist()
+# title_keywords_vector = title_keyword_vector_generator(articles_list)
+# print title_keywords_vector
+
 #
 
-fdist_content = articles_list[0].content_freqDist()
 # print "len articles_list[0].freqDist: ", len(fdist_content.items())
-# all_fdist = content_keyword_vector_generator(articles_list)
+all_fdist = content_keyword_vector_generator(articles_list)
 # print "len all_fdist: ", len(all_fdist)
-# all_fdist.plot(100, title="hello", cumulative=False)
-# print "top 20 Frequency:\n", all_fdist.tabulate(20)
+all_fdist.plot(100, title="hello", cumulative=False)
+print "top 20 Frequency:\n", all_fdist.tabulate(20)
+
 
 
 # print len(articles_list)
--- lab1_sgm.py
+++ lab1_sgm.py
@@ -13,77 +13,76 @@ from lxml import etree
 # print etree.tostring(root, pretty_print=True)
 
 class RawArticle:
-	def __init__(self, title, content, topic):
-		self.title = title
-		self.content = content
-		self.topic = topic
+    def __init__(self, title, content, topic):
+        self.title = title
+        self.content = content
+        self.topic = topic
 
 
 class SgmFile:
-	articles = []
+    def __init__(self, filename):
+        self.articles = []
+        def hasTopic(node):
+            return node.get('topics').lower() == "yes" and node.find('topics').find('d') != None
+        def hasContent(node):
+            return node.find('text').find('content') != None
+        def hasTitle(node):
+            return node.find('text').find('title') != None
 
-	def __init__(self, filename):
-		def hasTopic(node):
-			return node.get('topics').lower() == "yes" and node.find('topics').find('d') != None
-		def hasContent(node):
-			return node.find('text').find('content') != None
-		def hasTitle(node):
-			return node.find('text').find('title') != None
+        root = html.parse(filename).getroot()
 
-		root = html.parse(filename).getroot()
+        # print etree.tostring(root, pretty_print=True)
 
-		# print etree.tostring(root, pretty_print=True)
+        # the first article is warpped into a 'body' tag
+        # so that we need to process it first
+        # reuters = root.find('body').getchildren()[0]
+        # title = ""
+        # content = ""
+        # if hasTitle(reuters):
+        # 	title = reuters.find('text').find('title').text
+        # # else:
+        # # 	print "no title, line#:", reuters.sourceline
+        # if hasContent(reuters):
+        # 	content = reuters.find('text').find('content').text
+        # # else:
+        # # 	print title
+        # ra0 = RawArticle(title, content) # raw article 0
+        # # print "topic :", reuters.get('topics')
+        # if reuters.get('topics').lower() == "yes":
+        # 	topic = reuters.find('topics').find('d').text
+        # 	ra0.topic = topic
+        # self.articles.append(ra0)
 
-		# the first article is warpped into a 'body' tag
-		# so that we need to process it first
-		# reuters = root.find('body').getchildren()[0]
-		# title = ""
-		# content = ""
-		# if hasTitle(reuters):
-		# 	title = reuters.find('text').find('title').text
-		# # else:
-		# # 	print "no title, line#:", reuters.sourceline
-		# if hasContent(reuters):
-		# 	content = reuters.find('text').find('content').text
-		# # else:
-		# # 	print title
-		# ra0 = RawArticle(title, content) # raw article 0
-		# # print "topic :", reuters.get('topics')
-		# if reuters.get('topics').lower() == "yes":
-		# 	topic = reuters.find('topics').find('d').text
-		# 	ra0.topic = topic
-		# self.articles.append(ra0)
-
-		ras = root.find('body').getchildren() # raw articles
-		# print "ras type: ", type(ras), "; len=", len(ras)
-		for reuter in ras:
-			# print "type reuter: ", type(reuter), "; tag=", reuter.tag
-			# if reuter.tag == 'body':
-			# 	# the first article is warpped into a 'body' tag
-			# 	# so that we need to get its child
-			# 	reuter = reuter.getchildren()[0]
-			title = ""
-			content = ""
-			topic = ""
-			if hasTitle(reuter):
-				title = reuter.find('text').find('title').text
-			# else:
-			# 	print "no title, line#:", reuter.sourceline
-			if hasContent(reuter):
-				content = reuter.find('text').find('content').text
-			# else:
-			# 	print title
-			if hasTopic(reuter):
-				# this is for the case that only has one topic word
-				# topic = reuter.find('topics').find('d').text
-				# ra.topic = topic
-				# in many cases, there are many words
-				for t in reuter.find('topics').itertext():
-					topic = topic + " " + t
-				# allTopic = reuter.find('topics').findall('d')
-				# print "type allTopic: ", type(allTopic)
-			ra = RawArticle(title, content, topic)
-			self.articles.append(ra)
+        ras = root.find('body').getchildren() # raw articles
+        # print "ras type: ", type(ras), "; len=", len(ras)
+        for reuter in ras:
+            # print "type reuter: ", type(reuter), "; tag=", reuter.tag
+            # if reuter.tag == 'body':
+            # 	# the first article is warpped into a 'body' tag
+            # 	# so that we need to get its child
+            # 	reuter = reuter.getchildren()[0]
+            title = ""
+            content = ""
+            topic = ""
+            if hasTitle(reuter):
+                title = reuter.find('text').find('title').text
+            # else:
+            # 	print "no title, line#:", reuter.sourceline
+            if hasContent(reuter):
+                content = reuter.find('text').find('content').text
+            # else:
+            # 	print title
+            if hasTopic(reuter):
+                # this is for the case that only has one topic word
+                # topic = reuter.find('topics').find('d').text
+                # ra.topic = topic
+                # in many cases, there are many words
+                for t in reuter.find('topics').itertext():
+                    topic = topic + " " + t
+                # allTopic = reuter.find('topics').findall('d')
+                # print "type allTopic: ", type(allTopic)
+            ra = RawArticle(title, content, topic)
+            self.articles.append(ra)
 
 
 # this piece of code shows how to use the above SgmFile class
